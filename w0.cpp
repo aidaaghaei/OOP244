12c9270d8e23b252a841b22755d48e802189541f
aidaaghaei <1aidaaghaie@gmail.com>
2026-01-12 14:01:07 -0500

    Add initial implementation of w0.cpp

---
+++ w0.cpp
@@ -0,0 +1,20 @@
+// OOP Workshop 0
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+// insert code here...
+std::cout << "#### Workshop Zero ####\n";
+std::cout << "Subject and Section:\n";
+std::cout << "OOP244NCCL\n";
+std::cout << "Name:\n";
+std::cout << ":\n";Aida Aghaei
+std::cout << "Student Number:\n";
+std::cout << "\n";109047258
+std::cout << "Email:\n";
+std::cout << "\n";aaghaei3@myseneca.ca
+
+
+return 0;
+
+}
